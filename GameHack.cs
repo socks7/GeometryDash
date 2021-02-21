c3c84b0260d3d66cea691e06741bfa2de78d66a8
socks <yair.maoz@gmail.com>
2021-02-21 17:30:10 +0200

    Add files via upload

---
+++ GameHack.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+
+namespace _
+{
+    public class GameHack //IMPORTANT: ONLY works on x64 builds
+    {
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory(int hProcess,
+        Int64 lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out int lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll")]
+        public static extern Int32 CloseHandle(IntPtr hProcess);
+
+        IntPtr processHandle;
+        Process process;
+        public long addr;
+        long[] offs;
+
+        public byte[] getByteArray()
+        {
+            int bytesRead1 = 0;
+            byte[] buffer1 = new byte[4];
+            ReadProcessMemory((int)processHandle, addr, buffer1, buffer1.Length, ref bytesRead1);
+            return buffer1;
+        }
+
+        public byte[] getByteArray(int size)
+        {
+            int bytesRead1 = 0;
+            byte[] buffer1 = new byte[size];
+            ReadProcessMemory((int)processHandle, addr, buffer1, buffer1.Length, ref bytesRead1);
+            return buffer1;
+        }
+
+        public float getDataAsNumber(int size)
+        {
+            return Convert.ToSingle(BitConverter.ToDouble(getByteArray(size), 0));
+        }
+
+        public float getDataAsNumber()
+        {
+            return BitConverter.ToSingle(getByteArray(), 0);
+        }
+
+        public string getDataAsString(int size)
+        {
+            return BitConverter.ToString(getByteArray(size), 0);
+        }
+
+        public string getDataAsString()
+        {
+            return BitConverter.ToString(getByteArray(2), 0);
+        }
+
+        public void attach(string processName, long[] offsets)
+        {
+            offs = offsets;
+            try
+            {
+                process = Process.GetProcessesByName(processName)[0];
+            }
+            catch
+            {
+
+                return;
+            }
+            processHandle = process.Handle;
+            int bytesRead1 = 0;
+            byte[] buffer1 = new byte[4];
+            addr = (long)GetModuleBaseAddress(process.ProcessName, process.MainModule.ModuleName) + offsets[0];
+            ReadProcessMemory((int)processHandle, (addr), buffer1, buffer1.Length, ref bytesRead1);
+            addr = (long)(BitConverter.ToInt32(buffer1, 0));
+            for (int i = 1; i < offsets.Length - 1; i++)
+            {
+                int bytesRead = 0;
+                byte[] buffer = new byte[4];
+                addr += offsets[i];
+                ReadProcessMemory((int)processHandle, (addr), buffer, buffer.Length, ref bytesRead);
+                addr = (long)(BitConverter.ToInt32(buffer, 0));
+            }
+            addr += offsets[offsets.Length - 1];
+        }
+
+        public void reAttach()
+        {
+            int bytesRead1 = 0;
+            byte[] buffer1 = new byte[4];
+            addr = (long)GetModuleBaseAddress(process.ProcessName, process.MainModule.ModuleName) + offs[0];
+            ReadProcessMemory((int)processHandle, (addr), buffer1, buffer1.Length, ref bytesRead1);
+            addr = (long)(BitConverter.ToInt32(buffer1, 0));
+            for (int i = 1; i < offs.Length - 1; i++)
+            {
+                int bytesRead = 0;
+                byte[] buffer = new byte[4];
+                addr += offs[i];
+                ReadProcessMemory((int)processHandle, (addr), buffer, buffer.Length, ref bytesRead);
+                addr = (long)(BitConverter.ToInt32(buffer, 0));
+            }
+            addr += offs[offs.Length - 1];
+        }
+
+        public int WriteMem(object v)
+        {
+            try
+            {
+                int bytesWritten = 0;
+                WriteProcessMemory(processHandle, (IntPtr)addr, BitConverter.GetBytes((float)v), (uint)BitConverter.GetBytes((float)v).Length, out bytesWritten);
+                return 1;
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+
+        public int WriteBytes(byte[] bts)
+        {
+            try
+            {
+                int bytesWritten = 0;
+                WriteProcessMemory(processHandle, (IntPtr)addr, bts, (uint)bts.Length, out bytesWritten);
+                return 1;
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+
+        private IntPtr GetModuleBaseAddress(string processName, string moduleName)
+        {
+            Process process;
+            try
+            {
+                process = Process.GetProcessesByName(processName)[0];
+            }
+
+            catch (IndexOutOfRangeException)
+            {
+                throw new ArgumentException($"No process with name {processName} is currently running");
+            }
+            var module = process.Modules.Cast<ProcessModule>().SingleOrDefault(m => string.Equals(m.ModuleName, moduleName, StringComparison.OrdinalIgnoreCase));
+            return module?.BaseAddress ?? IntPtr.Zero;
+        }
+    }
+}
---
+++ gdGraphics.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _
+{
+    class gdGraphics
+    {
+        public GameHack[,] gameHooks;
+        public void makeGrid(int x, int y)
+        {
+            gameHooks = new GameHack[x,y];
+            for (int y1 = 0; y1<y;y1++)
+            {
+                for (int x1 = 0; x1<x; x1++)
+                {
+                    gameHooks[x1, y1] = new GameHack();
+                    gameHooks[x1, y1].attach("GeometryDash", new long[] { 0x003222D0, 0x164, 0x274, (long)(4 * (y1*x+x1)+4), 0x20, 0x8, 0x0, 0xD8 });
+                }
+            }
+        }
+
+        public void reAttach()
+        {
+            foreach (GameHack hook in gameHooks)
+            {
+                hook.reAttach();
+            }
+        }
+
+        public void setVisible(int x, int y, bool value)
+        {
+            gameHooks[x, y].WriteBytes(new byte[] { BitConverter.GetBytes(value)[0] });
+        }
+
+        public bool getVisible(int x, int y)
+        {
+            return Convert.ToBoolean(gameHooks[x, y].getByteArray()[0]);
+        }
+    }
+}
